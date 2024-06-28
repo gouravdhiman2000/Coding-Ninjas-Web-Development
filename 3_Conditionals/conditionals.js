676955f715a21fa9633be14261a55a3cea1257fb
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2024-06-28 19:59:18 +0530

    conditional loops, for, while and do-while loop

---
+++ 3_Conditionals/conditionals.js
@@ -0,0 +1,20 @@
+var a=-1;
+
+// if(a>0){
+//     console.log("Positive");
+// }
+// else if(a<0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("zero");
+// }
+
+
+
+if(a){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
