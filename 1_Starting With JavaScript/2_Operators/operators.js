9d111bfc3f6fcff5715f18c204e52eebc305e924
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2024-06-29 12:17:30 +0530

    operators in JS

---
+++ 1_Starting With JavaScript/2_Operators/operators.js
@@ -0,0 +1,63 @@
+
+// Arithmetic operaters
+var a = 10+2;
+console.log(a);
+
+var b= 13/2;
+console.log(b);
+
+var c= 15.5%2;
+console.log(c);
+
+// assignment operators
+var d =10;
+d+=12;
+console.log(d);
+
+// comparison operator
+var e= 100;
+e == 0;//false
+console.log(e);
+
+var f = "Hello" + "World";
+console.log(f);//hello world
+
+var g = "Hello" + 2;
+console.log(g);//Hello2-string
+
+var h  = "1"+1;
+console.log(h);//11-string
+
+//typeconversion in case of subtraction
+var i = '1'-1;
+console.log(i);//zero
+
+var i = '1'* 1;
+console.log(i);//one
+
+var i = '1'>0;
+console.log(i);//true
+
+var j= 1 == 1;
+console.log(j);//true
+
+var j= 1 == '1';
+console.log(j);//true- string is converted to no
+
+var j = 1 != '1';
+console.log(j);//false
+
+//compare on the basis of typeOf
+
+var j = 1 === 1;
+console.log(j);//true
+
+var j = 1 === '1';
+console.log(j);//false
+
+var j = 1 !== '1';
+console.log(j);//true
+
+
+
+
