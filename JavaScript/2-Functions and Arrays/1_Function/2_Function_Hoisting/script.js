1ea235dc6a5c88849cb933830d35c863bc3b21f2
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2024-07-02 12:50:35 +0530

    learn about function hoisting

---
+++ JavaScript/2-Functions and Arrays/1_Function/2_Function_Hoisting/script.js
@@ -0,0 +1,16 @@
+function hoistDemo(){
+    console.log(i);
+    // varible hoisting
+    var i=10;
+}
+hoistDemo();//undefined
+
+
+// function hoisting
+hoistDemo();//undefined
+
+function hoistDemo(){
+    console.log(i);
+    // varible hoisting
+    var i=10;
+}
